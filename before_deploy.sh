e0eebbc20527eef83c49338e0852152d500bb478
Max <minecraftlifelp@gmail.com>
2017-06-03 16:23:34 +0200

    fff

--- before_deploy.sh
+++ before_deploy.sh
@@ -1,2 +1,2 @@
-B="$(ls Connect-Four-Battle-of-AI-Core-v*-release.zip | cut -d'_' -f7)"
+B="$(find $TRAVIS_BUILD_DIR/target -maxdepth 1 -type f -name 'Connect-Four-Battle-of-AI-Core-v*-release.zip' | cut -d'_' -f7)"
 export GIT_TAG=$TRAVIS_BRANCH-$B
